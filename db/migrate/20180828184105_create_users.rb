89763dcb7d793807a9f4fd06e73aaa69c06b102d
Victoria Fluharty <victoria.fluharty@gmail.com>
2018-08-28 18:44:00 +0000

    automatically backed up by learn

--- db/migrate/20180828184105_create_users.rb
+++ db/migrate/20180828184105_create_users.rb
@@ -2,11 +2,11 @@ class CreateUsers < ActiveRecord::Migration
   def change
     create_table :users do |t|
       t.string :name
-      t.string :email 
-      t.string :image 
-      t.integer :uid 
+      t.string :email
+      t.string :image
+      t.integer :uid
 
-      t.timestamps null: false 
+      t.timestamps null: false
     end
   end
 end
